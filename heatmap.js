307a57ce74e61cd24108b3d5afdc88bcbd96940b
Chris Vann <chris@elserinteractive.com>
2015-09-14 15:34:58 -0500

    initial commit

---
+++ heatmap.js
@@ -0,0 +1,41 @@
+
+// simulate some data
+
+var values = []
+for (var state in states) {
+    values.push([state, Math.floor(Math.random()*5000)]);
+}
+
+// sort in descending order
+
+values.sort(function (a, b) {
+    return b[1] - a[1];
+});
+
+window.onload = function () {
+
+    var map = Raphael("container", 1000, 900),
+        color = Raphael.rgb2hsb(0,102,255)
+        attr = {
+            "fill": "#d3d3d3",
+            "stroke": "#fff",
+            "stroke-opacity": "1",
+            "stroke-linejoin": "round",
+            "stroke-miterlimit": "4",
+            "stroke-width": "0.75",
+            "stroke-dasharray": "none"
+        };
+
+    for (var i=0; i<values.length; i++) {
+        var a = values.length - i,
+            b = 255.0 / values.length,
+            c = JSON.parse(JSON.stringify(attr)),
+            d = a/values.length;
+
+        c.fill = Raphael.hsb(color.h, d, color.b);
+        map.path(states[values[i][0]]).attr(c);
+    }
+  };
+
+
+
